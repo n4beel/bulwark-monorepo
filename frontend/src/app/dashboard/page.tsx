1d49ebd954c2bfb15fc9a89f874ab554e136fa39
77-ahmad-collab <ahmaq098@gmail.com>
2025-10-28 21:57:47 +0200

    feat: refactor dashboard page structure and extract content into separate component

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,39 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import DashboardPageContent from "@/components/Dashboard/DageboardPageContent";
+import { Suspense } from "react";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { RootState } from "@/store/store";
-import DashboardNavbar from "@/components/Dashboard/DashboardNavBar";
-import DashboardHero from "@/components/Dashboard/Hero";
-import Loading from "@/components/Loading";
-import AuthModal from "@/components/auth/AuthModal";
-import { logout } from "@/store/slices/authSlice";
 
 export default function DashboardPage() {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const searchParams = useSearchParams();
-  const reportId = searchParams.get("report");
-  const { user, loading } = useSelector((state: RootState) => state.auth);
-
-  if (loading) {
-    return <Loading />;
-  }
   return (
-    <>
-      <main className="min-h-screen bg-[var(--background)]">
-        <DashboardNavbar />
-        <DashboardHero initialReportId={reportId || undefined} />
-      </main>
-      <AuthModal
-        open={!user}
-        onClose={() => {
-          router.push("/");
-        }}
-        shouldRedirect={false}
-      />
-    </>
+    <Suspense fallback={<div>Loading dashboard...</div>}>
+      <DashboardPageContent />
+    </Suspense>
   );
 }
